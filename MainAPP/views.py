e6c733049e215780003f0551a4614b2cfa52bcbb
yeehdavid <yeehdavid@163.com>
2017-08-17 23:51:50 +0800

    虎扑滚动新闻面板的添加

--- MainAPP/views.py
+++ MainAPP/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from MainAPP.models import Videos, Board_News,Latest_News,Board_Videos,Lx,Lx_Part
+from MainAPP.models import Videos, Board_News,Latest_News,Board_Videos,Lx,Lx_Part,Hoop_Latest_News
 
 # Create your views here.
 def index(request):
     B_N = Board_News.objects.all().order_by('-created_time')
     News_list0 = B_N[:1]#轮播的第一张
     News_list1 = B_N[1:5]#轮播的后面几张
-    News_list2 = Latest_News.objects.all().order_by('-created_time')[0:10]
-    return render(request, 'index.html', context={'News_list0':News_list0,'News_list1':News_list1,'News_list2':News_list2})
+    News_list2 = Latest_News.objects.all().order_by('-created_time')[0:11]
+    Hoop_News = Hoop_Latest_News.objects.all().order_by('-created_time')[0:33]
+    return render(request, 'index.html', context={'News_list0':News_list0,'News_list1':News_list1,'News_list2':News_list2,
+                                                  'Hoop_left':Hoop_News[0:11],'Hoop_center':Hoop_News[11:22],'Hoop_right':Hoop_News[22:33]})
 
 def videos(request):
     dic = dict()
