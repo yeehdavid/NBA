4b83b981ef231768f957de15f61a42c27805ecfc
yeehdavid <yeehdavid@163.com>
2017-08-23 22:09:14 +0800

    更新爬虫

--- spider/Main_Spider.py
+++ spider/Main_Spider.py
@@ -44,35 +44,35 @@ def Hoop_Latest_News():
     except:
         print('cant get the hoop html')
 
-    try:
 
 
 
-        for i in bsobj.find_all('a' ,href = re.compile('https://voice.hupu.com/nba/')):#获取最新动态
-            if 'target' not in i.attrs:
-                continue
 
-            else:
+    for i in bsobj.find_all('a' ,href = re.compile('https://voice.hupu.com/nba/')):#获取最新动态
+        if 'target' not in i.attrs:
+            continue
 
-                try:
-                    cur.execute("SELECT id FROM MainAPP_hoop_latest_news WHERE title = %s", (i.string))
-                    The_ID = cur.fetchone()[0]
+        else:
 
-                    #如果这条新闻不在set当中那就进行存储操作
-                    #此处代码将新闻主要信息存入数据库中
-                    #print(i.attrs['href'],i.string)
-                except:
-                    print('can`t get hoop——new,it is a new ,i will insert into my table')
-                    cur.execute("INSERT INTO MainAPP_hoop_latest_news (title,url,created_time) VALUES (%s,%s,%s)",
-                                (i.string, i.attrs['href'], datetime.datetime.now()))
-                    cur.connection.commit()
-                    print('insert success')
-                    #
-                    #print(time.time())
-                #print('插入数据完毕：', datetime.datetime.now())
-                break
-    except:
-        print('hoop_latest_news caozuo shibai')
+            try:
+                cur.execute("SELECT id FROM MainAPP_hoop_latest_news WHERE title = %s", (i.string))
+                The_ID = cur.fetchone()[0]
+
+                #如果这条新闻不在set当中那就进行存储操作
+                #此处代码将新闻主要信息存入数据库中
+                #print(i.attrs['href'],i.string)
+            except:
+                print('can`t get hoop——new,it is a new ,i will insert into my table')
+                cur.execute("INSERT INTO MainAPP_hoop_latest_news (title,url,created_time) VALUES (%s,%s,%s)",
+                            (i.string, i.attrs['href'], datetime.datetime.now()))
+                cur.connection.commit()
+                print('insert success')
+                #
+                #print(time.time())
+            #print('插入数据完毕：', datetime.datetime.now())
+            break
+
+
 
 
 
@@ -118,7 +118,7 @@ def NBA_Official_News():
                 break
 
     except:
-        print('cant caozuo shuju ')
+        print('cant NBA offical caozuo shuju ')
 
 
 
